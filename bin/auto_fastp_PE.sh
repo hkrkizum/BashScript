d8a5e1fd48894f6c792de9d0df2695b4d3f5af2f
hkrkizum <aoxora333@gmail.com>
2019-11-29 02:04:49 +0900

    modify

--- bin/auto_fastp_PE.sh
+++ bin/auto_fastp_PE.sh
@@ -29,28 +29,33 @@ cd $Output_path
 echo $Output_path
 
 for i in $(seq 1 ${Check_fileN1}); do
+	echo "---------------------------------------------------------"
 	echo "Readline:" $i
 	# i行目を取得
 	foword=`head -n $i $Iput_path_f_ab | tail -n 1`
 	reverse=`head -n $i $Iput_path_r_ab | tail -n 1`
+	echo "Foward: ${foword}"
+	echo "Foward: ${reverse}"
 
 	# gzファイルを展開
-	unpigz $foword
-	unpigz $reverse
-	echo "gunzip complete"
+	# echo "gunzip start........"
+	# unpigz $foword
+	# unpigz $reverse
+	# echo "gunzip complete"
 
 	# 共通名を取得
-	foldername=`basename $foword | perl -pe 's/\.fastq\.gz//'`
+	foldername=`basename $foword | perl -pe 's/_.\.fastq\.gz//'`
 	# 拡張子(.gz)を除いたファイル名(=解凍後のfastqファイル)名取得
-	foword_name=`basename ${foword:0:-3}`
-	reverse_name=`basename ${reverse:0:-3}`
+	# foword_name=`basename ${foword:0:-3}`
+	# reverse_name=`basename ${reverse:0:-3}`
 
+	echo "fastp start........."
 	# fastp実行
 	fastp \
-	--in1 ${foword:0:-3} \
-	--in2 ${reverse:0:-3} \
-	--out1 ${foword_name:0:-6}_Filtered.fastq \
-	--out2 ${reverse_name:0:-6}_Filtered.fastq \
+	--in1 ${foword} \
+	--in2 ${reverse} \
+	--out1 ${foword:0:-9}_Filtered.fastq \
+	--out2 ${reverse:0:-9}_Filtered.fastq \
 	--html ${foldername}.html \
 	--json ${foldername}.json \
 	--thread 8 \
@@ -58,16 +63,16 @@ for i in $(seq 1 ${Check_fileN1}); do
 	--trim_poly_x
 
 	# 実行後のファイルを圧縮
-	pigz *.fastq
+	# echo "trimmed fastq pigz start........"
+	# pigz *.fastq
+	# echo "trimmed fastq pigz complete"
 
 	#展開した元fastqを再圧縮
-	pigz ${foword:0:-3}
-	pigz ${reverse:0:-3}
-	echo "pigz complete, finesh"
-
+    # echo "fastq pigz start........"
+	# pigz ${foword:0:-3}
+	# pigz ${reverse:0:-3}
+	# echo "pigz complete, finesh"
 done
-
-
 
 cd $CurrentDir
 
