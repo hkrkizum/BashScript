f792e58c497ca2eace835c3234346032924bbc21
hkrkizum <aoxora333@gmail.com>
2019-12-03 20:14:06 +0900

    no message

--- bin/auto_STAR_PE.sh
+++ bin/auto_STAR_PE.sh
@@ -1,17 +1,58 @@
 #!/bin/bash
-while getopts g:f:r:o: OPT
+
+# Set param ------------------------------------------------
+export FILENAME=$(basename $0)
+CurrentDir=`pwd` # 現在のDir取得
+FLG_A=0
+FLG_B=0
+FLG_C=0
+FLG_D=0
+
+# Get arg --------------------------------------------------
+while getopts g:f:r:o:h OPT
 do
   case $OPT in
-  	"g" ) FLG_A="TRUE" ; Genome_path="$OPTARG" ;;
-    "f" ) FLG_B="TRUE" ; Iput_path_f="$OPTARG" ;;
-	"r" ) FLG_C="TRUE" ; Iput_path_r="$OPTARG" ;;
-    "o" ) FLG_D="TRUE" ; Output_path="$OPTARG" ;;
+  	"g" ) FLG_A=1 ; Genome_path="$OPTARG" ;;
+    "f" ) FLG_B=1 ; Iput_path_f="$OPTARG" ;;
+	"r" ) FLG_C=1 ; Iput_path_r="$OPTARG" ;;
+    "o" ) FLG_D=1 ; Output_path="$OPTARG" ;;
+	"h" ) echo "Usage: ${FILENAME} -g /path/to/genome/index/ -f /path/to/foward_read_list -r /path/to/reverse_read_list -o /path/to/outdir"
+	      exit 1 ;;
       * ) echo "Usage: $CMDNAME [-g VALUE] [-f VALUE] [-r VALUE] [-o VALUE] " 1>&2
           exit 1 ;;
   esac
 done
 
-CurrentDir=`pwd` # 現在のDir取得
+if [[ $FLG_A -eq 1 ]] && [[ $FLG_B -eq 1 ]] && [[ $FLG_C -eq 1 ]] && [[ $FLG_D -eq 1 ]];then
+	echo "option command validated"
+else
+	echo "wrong option number"
+	exit 1
+fi
+
+# validate files --------------------------------------------------
+if [[ ! -d $Genome_path ]]; then
+	echo "no Index dir"
+	exit 1
+fi
+
+if [[ ! -e $Iput_path_f ]]; then
+	echo "no list_f file"
+	exit 1
+fi
+
+if [[ ! -e $Iput_path_r ]]; then
+	echo "no list_r file"
+	exit 1
+fi
+
+mkdir -p $Output_path/
+if [[ ! -d $Output_path ]]; then
+	echo "no output dir"
+	exit 1
+fi
+
+# validate fastq.gz ------------------------------------------------
 Iput_path_f_ab=`realpath $Iput_path_f`
 Iput_path_r_ab=`realpath $Iput_path_r`
 
@@ -24,49 +65,45 @@ else
 	echo "File number is not same:" $Check_fileN1 $Check_fileN2
 fi
 
-# アウトプット先の作成
-mkdir -p $Output_path/
-cd $Output_path
-echo $Output_path
-
 for i in $(seq 1 ${Check_fileN1}); do
 	echo "---------------------------------------------------------"
 	echo "Readline:" $i
-	# i行目を取得
+	# get row i file name
 	foword=`head -n $i $Iput_path_f_ab | tail -n 1`
 	reverse=`head -n $i $Iput_path_r_ab | tail -n 1`
 
-	# gzファイルを展開
+	# unpigz
 	echo "gunzip start........"
-	unpigz $foword
-	unpigz $reverse
+	unpigz -p 6 $foword
+	unpigz -p 6 $reverse
 	echo "gunzip complete"
 
-	# 共通名を取得
-	foldername=`basename $foword | perl -pe 's/\.fastq\.gz//'`
-	# 拡張子(.gz)を除いたファイル名(=解凍後のfastqファイル)名取得
+	# modifiy filename -- fastq.gz to fastq
 	foword_name=`basename ${foword:0:-3}`
 	reverse_name=`basename ${reverse:0:-3}`
-	echo $foword_name
-	echo $reverse_name
 
-	#STAR実行
+	echo "Input fastq: " $foword_name " " $reverse_name
+
+	# make output filename
+	foldername=${Output_path}/`basename $foword | perl -pe 's/\.fastq\.gz//'`
+	echo "Output file prefix: " $foldername
+
+	# run STAR
 	STAR \
 	--genomeDir ${Genome_path} \
 	--readFilesIn ${foword:0:-3} ${reverse:0:-3} \
-	--runThreadN 8 \
+	--runThreadN 6 \
  	--outSAMtype BAM SortedByCoordinate \
  	--outFileNamePrefix $foldername \
  	--outFilterMultimapNmax 1
 
-	#展開した元fastqを再圧縮
+	# compress fassq
 	echo "fastq pigz start........"
-	pigz ${foword:0:-3}
-	pigz ${reverse:0:-3}
+	pigz -p 6 ${foword:0:-3}
+	pigz -p 6 ${reverse:0:-3}
 	echo "pigz complete, finesh"
 
 done
 
 echo "finished"
 cd $CurrentDir
-
